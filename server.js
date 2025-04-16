1d36d4195892c69a3901d652c11e939b64f91084
Elsie866 <eknabwire@gmail.com>
2025-04-16 00:00:31 -0900

    get , post,connection to db(newpost)

--- server.js
+++ server.js
@@ -30,17 +30,39 @@ app.get("/register", (req, res) => {
 
 app.get("/posts", (req, res) => {
   connection.query("SELECT * FROM posts", (error, posts) => {
+    if (error) console.log(error);
     console.log(posts);
+    res.json(posts);
   });
-  res.render("posts.ejs"); // shows all posts with possible actions like delete,hide,view post,view comments,likes etc...actions will be  in form of links and buttons
+  // shows all posts with possible actions like delete,hide,view post,view comments,likes etc...actions will be  in form of links and buttons
 });
 
 app.get("/posts/:postID", (req, res) => {
   res.render("post.ejs"); //shows one/specific posts
 });
 
-app.post("/newPost", (req, res) => {
-  res.redirect("posts/5");
+app.get("/newpost", (req, res) => {
+  res.render("newpost.ejs");
+});
+
+app.post("/newpost", express.urlencoded({ extended: true }), (req, res) => {
+  console.log(req.body);
+  connection.query(
+    `INSERT INTO posts(user_id,title,content) VALUES(2,'${req.body.title}','${req.body.content}')`,
+    (err) => {
+      if (err) {
+        res.status(504);
+        res.json(err);
+      } else {
+        // res.send("Post created succesfully");
+        res.redirect("/posts");
+      }
+    }
+  );
+
+  // res.redirect("posts/5");
 });
 
 app.listen(8000, () => console.log("App started running on port 8000"));
+
+// node --watch <file>  Works like nodemon.Automatically restarts the server when new changes are saved.
